d7b62133d18ff9c5dce76504dfd19be5171677ea
Second <soltany.2019@gmail.com>
2021-09-08 11:42:02 +0430

    finish-javascript1

--- js/script.js
+++ js/script.js
@@ -1,67 +1,67 @@
-    let intervalHandle = 1;
-    let startTime = new Date();
+let intervalHandle = 1;
+let startTime = new Date();
 
 
-    const startButton = document.getElementById("start");
-    const resetButton = document.getElementById("reset");
-    const outputLabel = document.getElementById("output");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
+const outputLabel = document.getElementById("output");
 
-    let getIntervalString = (startTime, endTime) => {
-      const milliseconds = endTime - startTime;
+let getIntervalString = (startTime, endTime) => {
+  const milliseconds = endTime - startTime;
 
-      const hour = Math.floor(milliseconds / (3600 * 1000));
-      const minute = Math.floor((milliseconds % (3600 * 1000)) / (60 * 1000));
-      const second = Math.floor((milliseconds % (60 * 1000)) / 1000);
-      const fraction = Math.floor(milliseconds % 1000);
-      return `${String(hour).padStart(2, "0")}:${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}.${String(fraction).padStart(3, "0")}`;
-    }
+  const hour = Math.floor(milliseconds / (3600 * 1000));
+  const minute = Math.floor((milliseconds % (3600 * 1000)) / (60 * 1000));
+  const second = Math.floor((milliseconds % (60 * 1000)) / 1000);
+  const fraction = Math.floor(milliseconds % 1000);
+  return `${String(hour).padStart(2, "0")}:${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}.${String(fraction).padStart(3, "0")}`;
+}
 
-    let start = () => {
-      console.log("start");
+let start = () => {
+  console.log("start");
 
-      if (intervalHandle !== null)
-        return;
+  if (intervalHandle !== null)
+    return;
 
-      startTime = new Date();
+  startTime = new Date();
 
-      intervalHandle = setInterval(() => {
-        console.log("interval");
-        const currentTime = new Date();
-        const intervalText = getIntervalString(startTime, currentTime);
+  intervalHandle = setInterval(() => {
+    console.log("interval");
+    const currentTime = new Date();
+    const intervalText = getIntervalString(startTime, currentTime);
 
-        outputLabel.innerText = intervalText;
-      }, 100);
+    outputLabel.innerText = intervalText;
+  }, 100);
 
-      startButton.disabled = true;
-      resetButton.disabled = false;
+  startButton.disabled = true;
+  resetButton.disabled = false;
 
-      startButton.style.backgroundColor = "#00ff00";
-      resetButton.style.backgroundColor = "#00ff00";
-      // document. getElementById("output"). style. display = "none"; //hide.
+  startButton.style.backgroundColor = "#00ff00";
+  resetButton.style.backgroundColor = "#00ff00";
+  // document. getElementById("output"). style. display = "none"; //hide.
 
-    };
+};
 
-    let reset = () => {
-      console.log("reset");
-
-
-      if (intervalHandle === null)
-        return;
-
-      clearInterval(intervalHandle);
-
-      intervalHandle = null;
-
-      startButton.disabled = false;
-      resetButton.disabled = true;
-
-      startButton.style.backgroundColor = "#ff0000";
-      resetButton.style.backgroundColor = "#ff0000";
+let reset = () => {
+  console.log("reset");
 
 
-      // document. getElementById("output"). style. display = "block"; //show.
-    }
+  if (intervalHandle === null)
+    return;
 
-    reset();
+  clearInterval(intervalHandle);
 
-    outputLabel.innerText = getIntervalString(startTime, startTime);
+  intervalHandle = null;
+
+  startButton.disabled = false;
+  resetButton.disabled = true;
+
+  startButton.style.backgroundColor = "#ff0000";
+  resetButton.style.backgroundColor = "#ff0000";
+
+
+  // document. getElementById("output"). style. display = "block"; //show.
+}
+
+reset();
+
+outputLabel.innerText = getIntervalString(startTime, startTime);
