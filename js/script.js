61e700c28cadff0b83b0955a4945ea2d9fbed33e
Second <soltany.2019@gmail.com>
2021-09-08 18:54:35 +0430

    lastupdate

--- js/script.js
+++ js/script.js
@@ -1,76 +1,66 @@
-    let intervalHandle = 1;
-    let startTime = new Date();
+let intervalHandle = null;
+let startTime = new Date();
+
+const startButton = document.getElementById("start");
+const startButtonImage = document.getElementById("start-button-image");
+const stopButton = document.getElementById("stop");
+const stopButtonImage = document.getElementById("stop-button-image");
+const outputLabel = document.getElementById("output");
+
+const getIntervalText = (startTime, endTime) => {
+  const milliseconds = endTime - startTime;
+
+  const hour = Math.floor(milliseconds / (3600 * 1000));
+  const minute = Math.floor((milliseconds % (3600 * 1000)) / (60 * 1000));
+  const second = Math.floor((milliseconds % (60 * 1000)) / 1000);
+  const fraction = Math.floor(milliseconds % 1000);
+
+  return `${String(hour).padStart(2, "0")}:${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}.${String(fraction).padStart(3, "0")}`;
+}
+
+const updateTime = () => {
+  const currentTime = new Date();
+  outputLabel.innerText = getIntervalText(startTime, currentTime);
+}
+
+const start = () => {
+  if (intervalHandle !== null)
+    return;
+
+  startTime = new Date();
+
+  intervalHandle = setInterval(updateTime, 7); // 7, so that the last digit seems to increment randomly
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  startButtonImage.src = "images/icon-04.png";
+  stopButtonImage.src = "images/icon-02.png";
+  document. getElementById("output"). style. display = "none"; //hide.
+
+};
+
+const stop = () => {
+  if (intervalHandle === null)
+    return;
+
+  clearInterval(intervalHandle);
+  intervalHandle = null;
+
+  updateTime(); // So that we have the most precise interval between Start and Stop
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  startButtonImage.src = "images/icon-01.png";
+  stopButtonImage.src = "images/icon-03.png";
+  document. getElementById("output"). style. display = "block"; //show.
+}
 
 
-    const startButton = document.getElementById("start");
-    const resetButton = document.getElementById("reset");
-    const outputLabel = document.getElementById("output");
-
-    let getIntervalString = (startTime, endTime) => {
-      const milliseconds = endTime - startTime;
-
-      const hour = Math.floor(milliseconds / (3600 * 1000));
-      const minute = Math.floor((milliseconds % (3600 * 1000)) / (60 * 1000));
-      const second = Math.floor((milliseconds % (60 * 1000)) / 1000);
-      const fraction = Math.floor(milliseconds % 1000);
-      return `${String(hour).padStart(2, "0")}:${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}.${String(fraction).padStart(3, "0")}`;
-    }
-
-    let start = () => {
-      console.log("start");
-      var image = document.getElementById('myImg')
-      var image1= document.getElementById('myImg1')
-
-      if (intervalHandle !== null && image.src.match("../images/icon-04.png"))
-        return;
-        image.src = "../images/icon-01.png";
-        image1.src = "../images/icon-02.png";
-
-      startTime = new Date();
-
-      intervalHandle = setInterval(() => {
-        console.log("interval");
-        const currentTime = new Date();
-        const intervalText = getIntervalString(startTime, currentTime);
-
-        outputLabel.innerText = intervalText;
-      }, 100);
-
-      startButton.disabled = true;
-      resetButton.disabled = false;
-
-      startButton.style.backgroundColor = "#00ff00";
-      resetButton.style.backgroundColor = "#00ff00";
-      // document. getElementById("output"). style. display = "none"; //hide.
-
-    };
-
-    let reset = () => {
-      var image = document.getElementById('myImg')
-      var image = document.getElementById('myImg1')
-
-      console.log("reset");
 
 
-      if (intervalHandle === null)
-        return;
-
-      clearInterval(intervalHandle);
-
-      intervalHandle = null;
-
-      startButton.disabled = false;
-      resetButton.disabled = true;
-
-      startButton.style.backgroundColor = "#ff0000";
-      resetButton.style.backgroundColor = "#ff0000";
-      image.src = "../images/icon-04.png"
-      image1.src = "../images/icon-03.png"
 
 
-      // document. getElementById("output"). style. display = "block"; //show.
-    }
+    
 
-    reset();
-
-    outputLabel.innerText = getIntervalString(startTime, startTime);
